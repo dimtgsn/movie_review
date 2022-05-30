3bf33a289a68824063b42502facdab5dcfe6098c
dmitry1721 <gasanyandmitry@yandex.ru>
2022-05-30 16:58:32 +0300

    Update static файлы и вёрстку

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Category, Actor, RatingStar, Rating, Genre, Movie, MovieShots, Reviews
@@ -14,3 +14,10 @@ class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+
+class AddReview(View):
+    """ Добавление отзыва"""
+    def post(self, request, pk):
+        print(request.POST)
+        return redirect("/")
