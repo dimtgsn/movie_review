dcd6b2f182ad49c8e6e357abdbe98b80cbd310c7
dmitry1721 <gasanyandmitry@yandex.ru>
2022-05-30 13:44:05 +0300

    Update static файлы и вёрстку

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Category, Actor, RatingStar, Rating, Genre, Movie, MovieShots, Reviews
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {"movie": movie})
+    model = Movie
+    slug_field = "url"
